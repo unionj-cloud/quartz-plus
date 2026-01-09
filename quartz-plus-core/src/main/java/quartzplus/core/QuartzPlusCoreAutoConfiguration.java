02772b8c4e759af69ae3c5e087bd5a58cf408e91
wubin1989 <328454505@qq.com>
2026-01-09 11:42:21 +0800

    fix: migrate auto generate schema feature from org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration

--- quartz-plus-core/src/main/java/quartzplus/core/QuartzPlusCoreAutoConfiguration.java
+++ quartz-plus-core/src/main/java/quartzplus/core/QuartzPlusCoreAutoConfiguration.java
@@ -17,17 +17,28 @@
 package quartzplus.core;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.quartz.QuartzProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
+import org.springframework.boot.autoconfigure.quartz.*;
+import org.springframework.boot.autoconfigure.sql.init.OnDatabaseInitializationCondition;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.sql.init.dependency.DatabaseInitializationDependencyConfigurer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
+import org.springframework.transaction.PlatformTransactionManager;
 import quartzplus.core.service.QuartzPlusBaseService;
 import quartzplus.core.service.impl.DefaultQuartzPlusBaseServiceImpl;
 
+import javax.sql.DataSource;
+
 @Configuration
-@Import(QuartzProperties.class)
+@EnableConfigurationProperties(QuartzProperties.class)
 @RequiredArgsConstructor
 public class QuartzPlusCoreAutoConfiguration {
 
@@ -37,5 +48,62 @@ public class QuartzPlusCoreAutoConfiguration {
     @ConditionalOnMissingBean
     public QuartzPlusBaseService getQuartzPlusBaseService() {
         return new DefaultQuartzPlusBaseServiceImpl(context);
+    }
+
+    @Configuration(proxyBeanMethods = false)
+    @ConditionalOnSingleCandidate(DataSource.class)
+    @ConditionalOnProperty(prefix = "spring.quartz", name = "job-store-type", havingValue = "jdbc")
+    @Import(DatabaseInitializationDependencyConfigurer.class)
+    protected static class JdbcStoreTypeConfiguration {
+
+        @Bean
+        @Order(0)
+        public SchedulerFactoryBeanCustomizer dataSourceCustomizer(QuartzProperties properties, DataSource dataSource,
+                                                                   @QuartzDataSource ObjectProvider<DataSource> quartzDataSource,
+                                                                   ObjectProvider<PlatformTransactionManager> transactionManager,
+                                                                   @QuartzTransactionManager ObjectProvider<PlatformTransactionManager> quartzTransactionManager) {
+            return (schedulerFactoryBean) -> {
+                DataSource dataSourceToUse = getDataSource(dataSource, quartzDataSource);
+                schedulerFactoryBean.setDataSource(dataSourceToUse);
+                PlatformTransactionManager txManager = getTransactionManager(transactionManager,
+                    quartzTransactionManager);
+                if (txManager != null) {
+                    schedulerFactoryBean.setTransactionManager(txManager);
+                }
+            };
+        }
+
+        private DataSource getDataSource(DataSource dataSource, ObjectProvider<DataSource> quartzDataSource) {
+            DataSource dataSourceIfAvailable = quartzDataSource.getIfAvailable();
+            return (dataSourceIfAvailable != null) ? dataSourceIfAvailable : dataSource;
+        }
+
+        private PlatformTransactionManager getTransactionManager(
+            ObjectProvider<PlatformTransactionManager> transactionManager,
+            ObjectProvider<PlatformTransactionManager> quartzTransactionManager) {
+            PlatformTransactionManager transactionManagerIfAvailable = quartzTransactionManager.getIfAvailable();
+            return (transactionManagerIfAvailable != null) ? transactionManagerIfAvailable
+                : transactionManager.getIfUnique();
+        }
+
+        @Bean
+        @SuppressWarnings("deprecation")
+        @ConditionalOnMissingBean({QuartzDataSourceScriptDatabaseInitializer.class,
+            QuartzDataSourceInitializer.class})
+        @Conditional(QuartzPlusCoreAutoConfiguration.JdbcStoreTypeConfiguration.OnQuartzDatasourceInitializationCondition.class)
+        public QuartzDataSourceScriptDatabaseInitializer quartzDataSourceScriptDatabaseInitializer(
+            DataSource dataSource, @QuartzDataSource ObjectProvider<DataSource> quartzDataSource,
+            QuartzProperties properties) {
+            DataSource dataSourceToUse = getDataSource(dataSource, quartzDataSource);
+            return new QuartzDataSourceScriptDatabaseInitializer(dataSourceToUse, properties);
+        }
+
+        static class OnQuartzDatasourceInitializationCondition extends OnDatabaseInitializationCondition {
+
+            OnQuartzDatasourceInitializationCondition() {
+                super("Quartz", "spring.quartz.jdbc.initialize-schema");
+            }
+
+        }
     }
 }
